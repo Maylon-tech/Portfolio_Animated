3d3fb0fa37e6c724801b425deba4d6419f397c15
Maylon-tech <minatokiroisen77@gmail.com>
2023-05-23 19:41:16 +0900

    Config main sass styling

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.scss'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Header
+      </header>
+      <h1>Nemoto Dev </h1>
+
+    </div>
+  )
+}
+
+export default App
